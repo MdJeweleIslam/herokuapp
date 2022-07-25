44ef88839cdd6f768afe0b02429453512431e044
Md Jewele Islam <mdjeweleislam@gmail.com>
2022-07-25 22:51:01 +0600

    Bug Fixing

--- herokuapp/settings.py
+++ herokuapp/settings.py
@@ -11,6 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+import django_heroku
+
+
+BASE_DIR_old = os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,9 +122,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR_old, 'static_in_env')]
+STATIC_ROOT = os.path.join(BASE_DIR_old, 'static_root')
+MEDIA_ROOT = os.path.join(BASE_DIR_old, 'media_root')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
